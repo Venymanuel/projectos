dcdef9aca9e15c9757979ee2e7b61eb46927fa51
Venny Manuel <127542231+Venymanuel@users.noreply.github.com>
2023-05-02 16:24:16 +0100

    new update

--- MyMusic/javascript.js
+++ MyMusic/javascript.js
@@ -4,34 +4,20 @@
 let openMenu = document.querySelector('.menu-icon')
 let closeMenu = document.querySelector('.nav-menu-icon')
 let menu = document.querySelector('.navgation')
-let mainContenier = document.querySelector('.main-contenier')
-let boxContenier =document.querySelectorAll('.box-contenier')
-let musicDiplay = document.querySelector('.playSongsList')
-let principalImg = document.querySelector('#principal')
-let songsTema = document.querySelector('.songsTema')
-let playList =document.querySelector('.playList')
+let iframe = document.querySelector('iframe')
+let contenier = document.querySelector('.articleContenier')
 
-let troca = ()=>{
-    console.log('ola')
-    console.log(boxContenier[0])
-
+let  openIfram = ()=>{
+    iframe.classList.add('active')
+    contenier.style.display = 'none'
 }
+
 openMenu.addEventListener('click', ()=>{
     menu.classList.add('active')
 }
 )
 closeMenu.onclick = ()=>{
     menu.classList.remove('active')
-
-}
-function openBoxPlay1(){
-    musicDiplay.classList.add('active')
-
-    let contenier = music[0]
-
-    principalImg.src  = contenier.image;
-    songsTema.innerHTML = contenier.name
-    console.log(principalImg)
 
 }
 
