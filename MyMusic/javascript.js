e6d69bf74b9fe54f90e4f7ce98420feb4633f5a5
Venny Manuel <127542231+Venymanuel@users.noreply.github.com>
2023-05-02 14:20:04 +0100

    update

--- MyMusic/javascript.js
+++ MyMusic/javascript.js
@@ -1,7 +1,21 @@
+
+// selecionando todas as variaveis
+
 let openMenu = document.querySelector('.menu-icon')
 let closeMenu = document.querySelector('.nav-menu-icon')
 let menu = document.querySelector('.navgation')
+let mainContenier = document.querySelector('.main-contenier')
+let boxContenier =document.querySelectorAll('.box-contenier')
+let musicDiplay = document.querySelector('.playSongsList')
+let principalImg = document.querySelector('#principal')
+let songsTema = document.querySelector('.songsTema')
+let playList =document.querySelector('.playList')
 
+let troca = ()=>{
+    console.log('ola')
+    console.log(boxContenier[0])
+
+}
 openMenu.addEventListener('click', ()=>{
     menu.classList.add('active')
 }
@@ -10,3 +24,17 @@ closeMenu.onclick = ()=>{
     menu.classList.remove('active')
 
 }
+function openBoxPlay1(){
+    musicDiplay.classList.add('active')
+
+    let contenier = music[0]
+
+    principalImg.src  = contenier.image;
+    songsTema.innerHTML = contenier.name
+    console.log(principalImg)
+
+}
+
+
+
+
---
+++ MyMusic/script.js
@@ -0,0 +1,26 @@
+
+
+let name1 = document.querySelector('.nama1')
+
+let music =[
+    {
+        name:"billie_eilish",
+        title:"i_love_you",
+        image:"img/04da469dd7be4dab96659aa1fa9f0ac9.jpeg",
+        music1:"sond/billie_eilish_i_love_you.mp3",
+        music2:"sond/billie_eilish_male_fantasy.mp3",
+        music4:"sond/billie_eilish_i_love_you.mp3",
+        music5:"sond/billie_eilish_male_fantasy.mp3",
+        music6:"sond/billie_eilish_i_love_you.mp3",
+        music7:"sond/billie_eilish_male_fantasy.mp3",
+        music8:"sond/billie_eilish_i_love_you.mp3",
+        music9:"sond/billie_eilish_male_fantasy.mp3",
+        music10:"sond/billie_eilish_i_love_you.mp3",
+        music11:"sond/billie_eilish_male_fantasy.mp3",
+        music12:"sond/billie_eilish_i_love_you.mp3",
+        music13:"sond/billie_eilish_male_fantasy.mp3",
+
+    },
+    {
+    }
+]
